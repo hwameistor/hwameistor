f307e491e0de8dcdf8f9869d70ac35a5edbaac59
mingming.zhou <mingling.zhou@daocloud.io>
2022-09-28 16:49:56 +0800

    fix: comment vgmigrate and vgconvert controller

--- pkg/local-storage/controller/add_localvolumegroupmigrate.go
+++ pkg/local-storage/controller/add_localvolumegroupmigrate.go
@@ -1,8 +1,6 @@
 package controller
 
-import "github.com/hwameistor/hwameistor/pkg/local-storage/controller/localvolumegroupmigrate"
-
 func init() {
 	// AddToManagerFuncs is a list of functions to create controllers and add them to a manager.
-	AddToManagerFuncs = append(AddToManagerFuncs, localvolumegroupmigrate.Add)
+	//AddToManagerFuncs = append(AddToManagerFuncs, localvolumegroupmigrate.Add)
 }
