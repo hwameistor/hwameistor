4facb6fbf6fd18a9e238cd600408bec6b4f44d11
mingming.zhou <mingling.zhou@daocloud.io>
2022-10-12 16:21:16 +0800

    [build] support scan image with trivy

---
+++ build/util/scan-image-with-trivy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+image_name=$1
+arch_list=("amd64" "arm64")
+severity="CRITICAL"
+e_code=${2:-0}
+
+for arch in ${arch_list[@]}
+do
+    img_of_cur_arch=${image_name}-${arch}
+    echo "# scan image for ${img_of_cur_arch}"
+
+    # check image
+    set +e
+    docker inspect ${img_of_cur_arch} 2>&1 > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "[Warning] No ${arch} image found on local , image name is ${img_of_cur_arch}. aborting  image manifest merge"
+        exit 2
+    fi
+    set -e
+    # scan image
+    echo "# scaning image ${image_name}..."
+    trivy image ${img_of_cur_arch} --severity ${severity} --exit-code ${e_code}
+
+    # report
+    echo "# scan successfully for image ${image_name}. No $severity level vulnerabilities found to be fixed."
+done
