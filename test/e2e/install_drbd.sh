d5cd760f96f18e8557636ddef135f9d04622e01d
123 <123>
2022-11-22 13:38:09 +0800

    [test]update eviction test

--- test/e2e/install_drbd.sh
+++ test/e2e/install_drbd.sh
@@ -1,4 +1,5 @@
-helm repo add hwameistor  http://hwameistor.io/hwameistor
-helm repo update hwameistor
-helm pull hwameistor/drbd-adapter --untar
-helm install drbd-adapter ./drbd-adapter -n hwameistor --create-namespace --set registry=daocloud.io/daocloud
+helm repo add addon 	https://release.daocloud.io/chartrepo/addon
+helm repo update addon
+helm pull addon/drbd-adapter --untar
+sed -i '30,36d' drbd-adapter/values.yaml
+helm install drbd-adapter ./drbd-adapter -n hwameistor --create-namespace --set registry=daocloud.io/daocloud --set deployOnMasters=yes
