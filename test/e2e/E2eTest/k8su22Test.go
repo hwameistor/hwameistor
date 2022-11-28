a5ce563628d9b4ba14b001e1e951659bd4d21139
123 <123>
2022-11-28 13:34:05 +0800

    [test]update ad test

--- test/e2e/E2eTest/k8su22Test.go
+++ test/e2e/E2eTest/k8su22Test.go
@@ -25,7 +25,7 @@ import (
 )
 
 var _ = ginkgo.Describe("test localstorage volume ", ginkgo.Label("k8su2204"), func() {
-
+	startAdRollback("k8su2204")
 	f := framework.NewDefaultFramework(clientset.AddToScheme)
 	client := f.GetClient()
 	ctx := context.TODO()
