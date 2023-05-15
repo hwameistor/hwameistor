4f2297763167401d7668d733c414287ac21ee714
Vacant2333 <vacant2333@gmail.com>
2023-05-15 18:05:37 +0800

    implement auth

--- pkg/apiserver/controller/AuthController.go
+++ pkg/apiserver/controller/AuthController.go
@@ -2,7 +2,9 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/hwameistor/hwameistor/pkg/apiserver/api"
 	"github.com/hwameistor/hwameistor/pkg/apiserver/manager"
+	"net/http"
 )
 
 type IAuthController interface {
@@ -19,9 +21,28 @@ func NewAuthController(m *manager.ServerManager) IAuthController {
 }
 
 func (n *AuthController) Auth(ctx *gin.Context) {
-
+	var req api.AuthReqBody
+	if err := ctx.ShouldBind(&req); err != nil {
+		ctx.JSON(http.StatusInternalServerError, api.RspFailBody{
+			ErrCode: 500,
+			Desc:    "Authentication Failed" + err.Error(),
+		})
+		return
+	}
+	if authResult := n.m.AuthController().Auth(req); authResult {
+		// todo: token? uuid?
+		ctx.JSON(http.StatusOK, api.AuthRspBody{
+			Success: true,
+			Token:   "??",
+		})
+		return
+	}
+	ctx.JSON(http.StatusUnauthorized, api.RspFailBody{
+		ErrCode: 401,
+		Desc:    "Fail to authenticate",
+	})
 }
 
 func (n *AuthController) Logout(ctx *gin.Context) {
-
+	// delete token
 }
