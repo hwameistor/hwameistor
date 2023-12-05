187ffcc90768fc6e040205ccc12b04b576465109
lancerxiu <42240182+hikariwo@users.noreply.github.com>
2023-12-05 20:30:38 +0800

    add more localdisknode test (#1293)
    Signed-off-by: lancerxiu <1377004871@qq.com>

--- pkg/local-disk-manager/builder/localdisknode/localdisknode_test.go
+++ pkg/local-disk-manager/builder/localdisknode/localdisknode_test.go
@@ -1,10 +1,13 @@
 package localdisknode
 
 import (
+	"github.com/hwameistor/hwameistor/pkg/apis/client/clientset/versioned/fake"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/tools/record"
 	"reflect"
 	"testing"
-
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"time"
 
 	"github.com/hwameistor/hwameistor/pkg/apis/hwameistor/v1alpha1"
 )
@@ -106,6 +109,154 @@ func TestBuild(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got, _ := tt.builder.Build(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Build() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+var (
+	fakeLocalDiskNodeName        = "local-disk-node-example"
+	fakeNodename                 = "10-6-118-10"
+	fakeFreeDiskCount            = int64(1)
+	fakeTotalDiskCount           = int64(1)
+	fakePoolClass                = "HDD"
+	fakePoolType                 = "REGULAR"
+	fakeLocalDiskNodeUID         = "local-disk-node-uid"
+	fakeTotalCapacityBytes int64 = 10 * 1024 * 1024 * 1024
+	fakeFreeCapacityBytes  int64 = 10 * 1024 * 1024 * 1024
+
+	LocalDiskNodeKind   = "LocalDiskNode"
+	LocalDiskKind       = "LocalDisk"
+	LeaseKind           = "Lease"
+	LocalDiskClaimKind  = "LocalDiskClaim"
+	LocalDiskVolumeKind = "LocalDiskVolume"
+
+	apiversion    = "hwameistor.io/v1alpha1"
+	fakeRecorder  = record.NewFakeRecorder(100)
+	fakeTimestamp = time.Now()
+)
+
+func CreateFakeKubeClient() (*Kubeclient, error) {
+	kubeclient := Kubeclient{}
+	clientset := fake.NewSimpleClientset()
+	kubeclient.SetClient(clientset)
+	return &kubeclient, nil
+}
+
+func GenFakeLocalDiskNodeObject() *v1alpha1.LocalDiskNode {
+	ldn := &v1alpha1.LocalDiskNode{}
+
+	typeMeta := v1.TypeMeta{
+		Kind:       LocalDiskNodeKind,
+		APIVersion: apiversion,
+	}
+
+	objectMeta := v1.ObjectMeta{
+		Name:              fakeLocalDiskNodeName,
+		ResourceVersion:   "",
+		UID:               types.UID(fakeLocalDiskNodeUID),
+		CreationTimestamp: v1.Time{fakeTimestamp},
+	}
+
+	spec := v1alpha1.LocalDiskNodeSpec{
+		NodeName: fakeLocalDiskNodeName,
+	}
+
+	status := v1alpha1.LocalDiskNodeStatus{
+		TotalCapacity: fakeTotalCapacityBytes,
+		FreeCapacity:  fakeFreeCapacityBytes,
+		FreeDisk:      fakeFreeDiskCount,
+		TotalDisk:     fakeTotalDiskCount,
+	}
+
+	ldn.Spec = spec
+	ldn.TypeMeta = typeMeta
+	ldn.ObjectMeta = objectMeta
+	ldn.Status = status
+
+	return ldn
+}
+
+func Test_KubeClient_Create(t *testing.T) {
+	testcases := []struct {
+		Description    string
+		DiskNode       *v1alpha1.LocalDiskNode
+		ExpectDiskNode *v1alpha1.LocalDiskNode
+	}{
+		// TODO: Add More test case
+		{
+			Description:    "It is a create localDiskNode test.",
+			DiskNode:       GenFakeLocalDiskNodeObject(),
+			ExpectDiskNode: GenFakeLocalDiskNodeObject(),
+		},
+	}
+
+	for _, testcase := range testcases {
+		t.Run(testcase.Description, func(t *testing.T) {
+			client, err := CreateFakeKubeClient()
+			if err != nil {
+				t.Fatal("create a fake client failed")
+			}
+			_, err = client.Create(testcase.DiskNode)
+			if err != nil {
+				t.Fatal("create a localDiskNode on fake client failed")
+			}
+			ldn, err := client.Get(testcase.DiskNode.Name)
+			if err != nil {
+				t.Fatal("get a specified localDiskNode on fake client failed")
+			}
+			if !reflect.DeepEqual(ldn, testcase.ExpectDiskNode) {
+				t.Fatal("get a localDiskNode not same as expectDiskNode")
+			}
+		})
+	}
+}
+
+func Test_KubeClient_Patch(t *testing.T) {
+	type args struct {
+		nodeName string
+	}
+	testcases := []struct {
+		Description    string
+		DiskNode       *v1alpha1.LocalDiskNode
+		modifyArgs     args
+		ExpectNodeName string
+	}{
+		// TODO: Add More test case
+		{
+			Description: "It is a patch localDiskNode test.",
+			DiskNode:    GenFakeLocalDiskNodeObject(),
+			modifyArgs: args{
+				nodeName: fakeNodename,
+			},
+			ExpectNodeName: fakeNodename,
+		},
+	}
+
+	for _, testcase := range testcases {
+		t.Run(testcase.Description, func(t *testing.T) {
+			client, err := CreateFakeKubeClient()
+			if err != nil {
+				t.Fatal("create a fake client failed")
+			}
+			// ensure the source ldn exists in storage
+			_, err = client.Create(testcase.DiskNode)
+			if err != nil {
+				t.Fatal("create a localDiskNode on fake client failed")
+			}
+			ldn := testcase.DiskNode.DeepCopy()
+			ldn.Spec.NodeName = testcase.modifyArgs.nodeName
+			err = client.Patch(testcase.DiskNode, ldn)
+			if err != nil {
+				// the origin object must exist in cluster
+				t.Fatal("patch a localDiskNode on fake client failed")
+			}
+			newLdn, err := client.Get(testcase.DiskNode.Name)
+			if err != nil {
+				t.Fatal("get a specified localDiskNode on fake client failed")
+			}
+			if !reflect.DeepEqual(testcase.ExpectNodeName, newLdn.Spec.NodeName) {
+				t.Fatal("get a localDiskNode not same as expectDiskNode")
 			}
 		})
 	}
