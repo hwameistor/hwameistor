28a0d7b670fe482faf4c3e6cfba0a880b6fcc14c
panyintian.fu <panyintian.fu@daocloud.io>
2024-05-17 11:24:46 +0800

    add 1.29 & 1.30 ad-test
    Signed-off-by: panyintian.fu <panyintian.fu@daocloud.io>
    Signed-off-by: peng9808 <peng.lai@daocloud.io>

--- test/e2e/ad_rollback.sh
+++ test/e2e/ad_rollback.sh
@@ -2,62 +2,73 @@
 # simple scripts mng machine
 # link hosts
 
-if [ $1 == "k8s1.24" ]; then
-  export GOVC_RESOURCE_POOL="fupan-adaptation-test"
-  export hosts="adaptation-master adaptation-node1 adaptation-node2"
-  export snapshot="k8s124"
-  kubectl config use-context k8s1.24
+if [ -z "$1" ]; then
+  echo "Usage: $0 {k8s1.24|k8s1.25|...}"
+  exit 1
 fi
 
-if [ $1 == "k8s1.25" ]; then
-  export GOVC_RESOURCE_POOL="fupan-adaptation-test"
-  export hosts="adaptation-master adaptation-node1 adaptation-node2"
-  export snapshot="k8s125"
-  kubectl config use-context k8s1.25
-fi
+# 函数：设置通用变量
+#set_common_vars() {
+#  export GOVC_RESOURCE_POOL="fupan-adaptation-test"
+#}
 
-if [ $1 == "k8s1.26" ]; then
-  export GOVC_RESOURCE_POOL="fupan-adaptation-test"
-  export hosts="adaptation-master adaptation-node1 adaptation-node2"
-  export snapshot="k8s126"
-  kubectl config use-context k8s1.26
-fi
-
-if [ $1 == "k8s1.27" ]; then
-  export GOVC_RESOURCE_POOL="fupan-adaptation-test"
-  export hosts="adaptation-master adaptation-node1 adaptation-node2"
-  export snapshot="k8s127"
-  kubectl config use-context k8s1.27
-fi
-if [ $1 == "k8s1.28" ]; then
-  export GOVC_RESOURCE_POOL="fupan-adaptation-test"
-  export hosts="adaptation-master adaptation-node1 adaptation-node2"
-  export snapshot="k8s128"
-  kubectl config use-context k8s1.28
-fi
-
-if [ $1 == "k8sc81" ]; then
-  export GOVC_RESOURCE_POOL="fupan-adaptation-test"
-  export hosts="fupan-ad-c81"
-  export snapshot="ad"
-  kubectl config use-context k8sc81
-fi
-
-if [ $1 == "k8su2204" ]; then
-  export GOVC_RESOURCE_POOL="fupan-adaptation-test"
-  export hosts="fupan-ad-u2204"
-  export snapshot="ad"
-  kubectl config use-context k8su2204
-fi
-
-
-
-if [ $1 == "centos7.9_offline" ]; then
-  export GOVC_RESOURCE_POOL="kylin10arm"
-  export hosts="fupan-ad-offline"
-  export snapshot="ad"
-  kubectl config use-context k8soffline
-fi
+# 使用 case 语句以简化逻辑并提高可读性
+case $1 in
+  k8s1.24)
+    export hosts="adaptation-master adaptation-node1 adaptation-node2"
+    export snapshot="k8s124"
+    kubectl config use-context k8s1.24
+    ;;
+  k8s1.25)
+    export hosts="adaptation-master adaptation-node1 adaptation-node2"
+    export snapshot="k8s125"
+    kubectl config use-context k8s1.25
+    ;;
+  k8s1.26)
+    export hosts="adaptation-master adaptation-node1 adaptation-node2"
+    export snapshot="k8s126"
+    kubectl config use-context k8s1.26
+    ;;
+  k8s1.27)
+    export hosts="adaptation-master adaptation-node1 adaptation-node2"
+    export snapshot="k8s127"
+    kubectl config use-context k8s1.27
+    ;;
+  k8s1.28)
+    export hosts="adaptation-master adaptation-node1 adaptation-node2"
+    export snapshot="k8s128"
+    kubectl config use-context k8s1.28
+    ;;
+  k8s1.29)
+    export hosts="adaptation-master adaptation-node1 adaptation-node2"
+    export snapshot="k8s129"
+    kubectl config use-context k8s1.29
+    ;;
+  k8s1.30)
+    export hosts="adaptation-master adaptation-node1 adaptation-node2"
+    export snapshot="k8s130"
+    kubectl config use-context k8s1.30
+    ;;
+  k8sc81)
+    export hosts="fupan-ad-c81"
+    export snapshot="ad"
+    kubectl config use-context k8sc81
+    ;;
+  k8su2204)
+    export hosts="fupan-ad-u2204"
+    export snapshot="ad"
+    kubectl config use-context k8su2204
+    ;;
+  centos7.9_offline)
+    export hosts="fupan-ad-offline"
+    export snapshot="ad"
+    kubectl config use-context k8soffline
+    ;;
+  *)
+    echo "Unsupported argument: $1"
+    exit 1
+    ;;
+esac
 
 export GOVC_INSECURE=1
 
