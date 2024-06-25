fd8e02436839083c5ab5278121afd24ae9f3be3b
panyintian.fu <panyintian.fu@daocloud.io>
2024-06-25 11:47:31 +0800

    e2e test
    Signed-off-by: panyintian.fu <panyintian.fu@daocloud.io>

--- test/pr-test.sh
+++ test/pr-test.sh
@@ -3,6 +3,10 @@
 go version
 echo $USER
 echo $PATH
+source /etc/profile
+go version
+echo $USER
+echo $PATH
 # Step3: go e2e test
 ginkgo -timeout=10h --fail-fast  --label-filter=${E2E_TESTING_LEVEL} test/e2e
 
