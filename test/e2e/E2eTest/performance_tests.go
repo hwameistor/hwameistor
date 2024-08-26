db80d59969c90a0a339b735e96455b76686aa8a6
panyintian.fu <panyintian.fu@daocloud.io>
2024-08-26 10:44:45 +0800

    [Test]Modify the image address
    Signed-off-by: panyintian.fu <panyintian.fu@daocloud.io>

--- test/e2e/E2eTest/performance_tests.go
+++ test/e2e/E2eTest/performance_tests.go
@@ -123,7 +123,7 @@ var _ = ginkgo.Describe("performance testing ", ginkgo.Label("performance-test")
 							Containers: []corev1.Container{
 								{
 									Name:  "web",
-									Image: "daocloud.io/daocloud/testtools:latest",
+									Image: "10.6.112.210/daocloud/testtools:latest",
 									Ports: []corev1.ContainerPort{
 										{
 											Name:          "http",
