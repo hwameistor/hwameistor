4ca2ed4d7b198d5c97c28a1763ce1176638281ca
panyintian.fu <panyintian.fu@daocloud.io>
2024-11-22 14:18:25 +0800

    [Test]Modify drbd related code in e2e test
    Signed-off-by: panyintian.fu <panyintian.fu@daocloud.io>

--- test/e2e/install_drbd.sh
+++ test/e2e/install_drbd.sh
@@ -1,7 +1,9 @@
-helm repo add drbd-adapter https://hwameistor.io/drbd-adapter/
-helm repo update drbd-adapter
-helm pull drbd-adapter/drbd-adapter --untar
-kubectl taint node --all node-role.kubernetes.io/master-
-helm install drbd-adapter ./drbd-adapter \
-    -n hwameistor --create-namespace \
-    --set imagePullPolicy=Always
+#helm repo add drbd-adapter https://hwameistor.io/drbd-adapter/
+#helm repo update drbd-adapter
+#helm pull drbd-adapter/drbd-adapter --untar
+#kubectl taint node --all node-role.kubernetes.io/master-
+#helm install drbd-adapter ./drbd-adapter \
+#    -n hwameistor --create-namespace \
+#    --set imagePullPolicy=Always
+
+echo "drbd is pre-installed"
