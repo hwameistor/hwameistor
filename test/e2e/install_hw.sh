1f65323e981830de74838e3206d0a5d8f56957a6
A_tian <76234281+FloatXD@users.noreply.github.com>
2026-01-04 16:24:17 +0800

    Merge pull request #1814 from FloatXD/e2e-test4
    test

--- test/e2e/install_hw.sh
+++ test/e2e/install_hw.sh
@@ -1,3 +1,4 @@
+echo "install hwameistor"
 helm repo add hwameistor https://hwameistor.io/hwameistor
 helm repo update hwameistor
 helm install hwameistor hwameistor/hwameistor    -n hwameistor --create-namespace --set global.k8sImageRegistry=m.daocloud.io/registry.k8s.io   --set global.hwameistorImageRegistry=ghcr.m.daocloud.io
